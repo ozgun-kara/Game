c3fc6895ebc5d89fa2b03d501c111899858065a2
Ozgun KARA <ozgunkara.devp@gmail.com>
2020-11-04 19:04:17 +0300

    Application settings are made

---
+++ Game/Game/Game.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Game
+{
+    partial class Game
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Game));
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Corbel", 10F, System.Drawing.FontStyle.Bold);
+            this.button1.Location = new System.Drawing.Point(207, 102);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Tahmin Yap";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.TahminYap_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.Font = new System.Drawing.Font("Consolas", 11.25F);
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(112, 160);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(291, 290);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "HEDEF";
+            this.columnHeader1.Width = 82;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "TAHMİN";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader2.Width = 86;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "SONUÇ";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader3.Width = 77;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(214, 61);
+            this.textBox1.MaxLength = 4;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(85, 25);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(214, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 19);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Tahmininiz";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Corbel", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(210, 465);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 41);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Yeni Oyun";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.YeniOyun_Click);
+            // 
+            // Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(511, 531);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.Name = "Game";
+            this.Text = "Game";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+    }
+}
+
---
+++ Game/Game/Game.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace Game
+{
+    public partial class Game : Form
+    {
+
+        int targetValue;
+
+        public Game()
+        {
+            Random rand = new Random(); 
+            targetValue = rand.Next(1000, 10000);   
+
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listView1.View = View.Details;
+        }
+
+        private void TahminYap_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text) || textBox1.Text.Length < 4)
+                return;
+
+            String targetValue1 = targetValue.ToString();
+            string inputValue = textBox1.Text;
+            List<String> outputList = new List<String>();
+
+            // Target 
+            char[] targetArray = new char[targetValue1.Length];
+
+            for (int i = 0; i < targetValue1.Length; i++)
+            {
+                targetArray[i] = targetValue1[i];
+            }
+
+
+            // Input
+            char[] inputArray = new char[inputValue.Length];
+
+            for (int i = 0; i < inputValue.Length; i++)
+            {
+                inputArray[i] = inputValue[i];
+            }
+
+            // First Character Control
+            if (inputArray[0] == targetArray[0])
+                outputList.Add("+");
+            else if (inputArray[0] == targetArray[1])
+                outputList.Add("-");
+            else if (inputArray[0] == targetArray[2])
+                outputList.Add("-");
+            else if (inputArray[0] == targetArray[3])
+                outputList.Add("-");
+            else
+                outputList.Add("X");
+
+            // Second Character Control
+            if (inputArray[1] == targetArray[1])
+                outputList.Add("+");
+            else if (inputArray[1] == targetArray[0])
+                outputList.Add("-");
+            else if (inputArray[1] == targetArray[2])
+                outputList.Add("-");
+            else if (inputArray[1] == targetArray[3])
+                outputList.Add("-");
+            else
+                outputList.Add("X");
+
+            // Third Character Control
+            if (inputArray[2] == targetArray[2])
+                outputList.Add("+");
+            else if (inputArray[2] == targetArray[0])
+                outputList.Add("-");
+            else if (inputArray[2] == targetArray[1])
+                outputList.Add("-");
+            else if (inputArray[2] == targetArray[3])
+                outputList.Add("-");
+            else
+                outputList.Add("X");
+
+            // Fourth Character Control
+            if (inputArray[3] == targetArray[3])
+                outputList.Add("+");
+            else if (inputArray[3] == targetArray[0])
+                outputList.Add("-");
+            else if (inputArray[3] == targetArray[1])
+                outputList.Add("-");
+            else if (inputArray[3] == targetArray[2])
+                outputList.Add("-");
+            else
+                outputList.Add("X");
+
+
+            string result = string.Join(" ", outputList);
+
+            ListViewItem item = new ListViewItem(targetValue1);
+            item.SubItems.Add(inputValue);
+            item.SubItems.Add(result);
+            listView1.Items.Add(item);
+            outputList.Clear();
+        }
+
+        private void YeniOyun_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void TextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
